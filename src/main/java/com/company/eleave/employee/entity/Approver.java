ac11afc3d722a5f986eb688d5c97b10c3885aebc
mariuszcrust <mariusz.danielewski@gmail.com>
2016-02-15 21:30:41 +0100

    Add configuration for web. Model is generated during startup

--- src/main/java/com/company/eleave/employee/entity/Approver.java
+++ src/main/java/com/company/eleave/employee/entity/Approver.java
@@ -4,17 +4,21 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Set;
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 
+import com.company.eleave.BaseEntity;
+
 /**
  *
  * @author Sebastian Szlachetka
  */
-public class Approver implements Serializable {
+@Entity
+public class Approver extends BaseEntity implements Serializable {
 
     @OneToOne
     @PrimaryKeyJoinColumn
