14c908ccbff10f2f76de1a00b2f4704be4942389
Sebastian Szlachetlka <sszlach@softserveinc.com>
2016-02-16 07:44:38 +0100

    Added Flyway without tests.

--- src/main/java/com/company/eleave/configuration/HibernateConfig.java
+++ src/main/java/com/company/eleave/configuration/HibernateConfig.java
@@ -2,11 +2,13 @@ package com.company.eleave.configuration;
 
 import java.util.Properties;
 import javax.sql.DataSource;
+import org.flywaydb.core.Flyway;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -27,7 +29,15 @@ public class HibernateConfig {
     @Autowired
     private Environment env;
 
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(dataSource());
+        return flyway;
+    }
+
     @Bean
+    @DependsOn("flyway")
     public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource());
