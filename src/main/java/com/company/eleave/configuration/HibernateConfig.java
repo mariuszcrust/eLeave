160a751121bfeeb280e3b47edeced0836e31b8da
Sebastian Szlachetlka <sszlach@softserveinc.com>
2016-02-23 09:01:32 +0100

    Spring repositories configuration.

--- src/main/java/com/company/eleave/configuration/HibernateConfig.java
+++ src/main/java/com/company/eleave/configuration/HibernateConfig.java
@@ -2,7 +2,7 @@ package com.company.eleave.configuration;
 
 import static com.company.eleave.configuration.HibernateConfig.REPOSITORY_PACKAGES;
 import java.util.Properties;
-
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 import org.hibernate.SessionFactory;
@@ -13,11 +13,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -66,6 +69,13 @@ public class HibernateConfig {
         factory.afterPropertiesSet();
 
         return factory.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory());
+        return txManager;
     }
 
     @Bean
