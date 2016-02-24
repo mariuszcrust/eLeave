16069f164afde9bc58315575d078ebd9c9e760fc
mariuszcrust <mariusz.danielewski@gmail.com>
2016-02-24 20:53:48 +0100

    try to set dbunit

--- src/main/it/com/company/eleave/employee/rest/ITEmployeeControllerRest.java
+++ src/main/it/com/company/eleave/employee/rest/ITEmployeeControllerRest.java
@@ -1,11 +1,6 @@
 package com.company.eleave.employee.rest;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.util.Assert;
-
-/*
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -14,26 +9,25 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-import org.springframework.util.Assert;
 
-import com.company.eleave.configuration.HibernateConfig;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
 import com.github.springtestdbunit.annotation.ExpectedDatabase;
-*/
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-//        DirtiesContextTestExecutionListener.class,
-//        TransactionalTestExecutionListener.class,
-//        DbUnitTestExecutionListener.class })
-//@DatabaseSetup("toDoData.xml")
-public class ITEmployeeControllerTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+      DirtiesContextTestExecutionListener.class,
+      TransactionalTestExecutionListener.class,
+      DbUnitTestExecutionListener.class })
+@ContextConfiguration("file:src/main/resources/test/context-test.xml")
+//@DbUnitConfiguration(databaseConnection={"dataSource"})
+@DatabaseSetup("toDoData.xml")
+public class ITEmployeeControllerRest {
 
-  //@Test
-  //@ExpectedDatabase("toDoData.xml")
-  public void findAll() throws Exception {
-      Assert.isTrue(true);
-  }
-  
+	  @Test
+	  @ExpectedDatabase("toDoData.xml")
+	  public void findAll() throws Exception {
+	      Assert.assertTrue(true);
+	  }
 }
