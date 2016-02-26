9be331b40d8672e04cd94d8f20eebc71f347db00
mariuszcrust <mariusz.danielewski@gmail.com>
2016-02-26 16:31:04 +0100

    add dummy test

--- src/test/java/com/company/eleave/employee/rest/TestEmployeeController.java
+++ src/test/java/com/company/eleave/employee/rest/TestEmployeeController.java
@@ -1,12 +1,26 @@
 package com.company.eleave.employee.rest;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
 import org.junit.Test;
+
+import com.company.eleave.employee.entity.Employee;
 
 public class TestEmployeeController {
 
 	@Test
 	public void dummyTest() {
-		Assert.assertTrue(true);
+		Employee employee = new Employee();
+		employee.setAnnualBalanceLeave(new ArrayList<>());
+		employee.setEmail("mail");
+		employee.setFirstName("john");
+		employee.setLastName("doe");
+		
+		Assert.assertEquals("mail", employee.getEmail());
+		Assert.assertEquals("john", employee.getFirstName());
+		Assert.assertEquals("doe", employee.getLastName());
+		Assert.assertTrue(employee.getAnnualBalanceLeave().size() == 0);
+		
 	}
 }
