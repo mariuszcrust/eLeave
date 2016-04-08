019052196b02130300e23e5113bda4dc2a58465c
Sebastian Szlachetlka <sszlach@softserveinc.com>
2016-04-08 10:13:46 +0200

    Remove xeditable and smart-table.

--- eLeave-ui/app/js/app.js
+++ eLeave-ui/app/js/app.js
@@ -1,14 +1,13 @@
 'use strict'
 
-var eLeaveApp = angular.module('eLeave',['ui.router', 'ui.bootstrap', 'ngAside', 'formly', 'smart-table', 'xeditable', 'ui.grid', 'ui.grid.edit', 'ncy-angular-breadcrumb', 'eLeave.home', 'eLeave.admin', 'eLeave.about', 'eLeave.controllers', 'eLeave.directives', 'eLeave.filters', 'eLeave.services']);
+var eLeaveApp = angular.module('eLeave',['ui.router', 'ui.bootstrap', 'ngAside', 'formly', 'ui.grid', 'ui.grid.edit', 'ncy-angular-breadcrumb', 'eLeave.home', 'eLeave.admin', 'eLeave.about', 'eLeave.controllers', 'eLeave.directives', 'eLeave.filters', 'eLeave.services']);
 
 eLeaveApp.config(['$urlRouterProvider', function($urlRouterProvider){
         $urlRouterProvider.otherwise('/home');
         $urlRouterProvider.deferIntercept();
 }]);
 
-eLeaveApp.run(['$state', '$urlRouter', 'editableOptions', function($state, $urlRouter, editableOptions){
-      editableOptions.theme = 'bs3';
+eLeaveApp.run(['$state', '$urlRouter', function($state, $urlRouter){
       $state.transitionTo('home');
       $urlRouter.listen();
 }]);
